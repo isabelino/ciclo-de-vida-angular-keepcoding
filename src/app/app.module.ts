f8063c3ccb3a76912bdd9fe49dce6c95d4fa2788
rolando <isa.klopez@hotmail.com>
2022-07-15 11:18:47 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component
   ],
   imports: [
     BrowserModule
